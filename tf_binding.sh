182556f7e00acfac9d6392cb7ee2394ead817806
Enching <e1luo@eng.ucsd.edu>
2016-01-14 11:34:53 -0800

    Add #PBS

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,4 +1,16 @@
 #PBS TAGS
+#!/bin/csh
+#PBS -q hotel
+#PBS -N tf_binding.sh
+#PBS -l nodes=10:ppn=2
+#PBS -l walltime=0:50:00
+#PBS -o output.sh
+#PBS -e error.txt
+#PBS -V
+#PBS -M e1luo@ucsd.edu
+#PBS -m abe
+#PBS -A e1luo
+
 cat tf.bed | awk '{if($4 =="NFKB") print}' > tf.nfkb.bed
 awk -F "\t" '{if($3 == "transcript") {print $0}}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf 
 #EXERCISE3 (include loading of biotools)
